9c617a283000395bed23a2d0a413cb28bdd4669d
Iuliia Dronova <jtyutereva@gmail.com>
2022-06-01 16:14:39 +0300

    isEnough function implemented

--- src/main/kotlin/CoffeeMachineApp.kt
+++ src/main/kotlin/CoffeeMachineApp.kt
@@ -28,28 +28,59 @@ fun prepareCoffee() {
         2 -> makeLatte()
         3 -> makeCappuccino()
     }
-    amountOfCups -= 1
+
     println()
 }
 
 fun makeEspresso() {
+    if (!isEnough(250, 16)) return
     amountOfWater -= 250
     amountOfCoffee -= 16
     amountOfMoney += 4
+    amountOfCups -= 1
 }
 
 fun makeLatte() {
+    if (!isEnough(350, 20, 75)) return
     amountOfWater -= 350
     amountOfMilk -= 75
     amountOfCoffee -= 20
     amountOfMoney += 7
+    amountOfCups -= 1
 }
 
 fun makeCappuccino() {
+    if (!isEnough(200, 12, 100)) return
     amountOfWater -= 200
     amountOfMilk -= 100
     amountOfCoffee -= 12
     amountOfMoney += 6
+    amountOfCups -= 1
+}
+
+fun isEnough(requiredWater: Int, requiredCoffee: Int, requiredMilk: Int = 0, requiredCups: Int = 1): Boolean {
+    var isActuallyEnough = true
+    if (amountOfWater < requiredWater) {
+        println("Sorry, not enough water!")
+        isActuallyEnough = false
+    }
+
+    if (amountOfMilk < requiredMilk) {
+        println("Sorry, not enough milk!")
+        isActuallyEnough = false
+    }
+
+    if (amountOfCoffee < requiredCoffee) {
+        println("Sorry, not enough coffee!")
+        isActuallyEnough = false
+    }
+
+    if (amountOfCups < requiredCups) {
+        println("Sorry, not enough cups!")
+        isActuallyEnough = false
+    }
+
+    return isActuallyEnough
 }
 
 fun replenishMachine() {
